a9d77e0014d1ca80769f61102c41aa366b427b2c
Mattressburn <chevyboxer@gmail.com>
2016-11-21 13:01:43 -0500

    set env vars and database vars

---
+++ server/config/cors,js.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Matthew on 11/21/16.
+ */
+const whitelist = [];
+
+module.exports = (req, res, next) => {
+    let allowCORS = false;
+
+    if (process.env.NODE_ENV !== 'production') allowCORS = true;
+    if (process.env.NODE_ENV === 'production' && whitelist.indexOf(req.headers.origin) !== -1) allowCORS = true;
+
+    if (!allowCORS) return next();
+
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    return next();
+};
